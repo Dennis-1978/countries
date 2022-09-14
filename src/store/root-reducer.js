ef41e4f5eed93b70a56077ce68771eb5bf59b839
Denis <d.khokhloff2011@yandex.ru>
2022-09-14 09:31:26 +0300

    created store

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,5 @@
-import {combineReducers} from 'redux';
-
-import {themeReducer} from './theme/theme-reducer'
+import { combineReducers } from 'redux';
 
 export const rootReducer = combineReducers({
-  theme: themeReducer,
+    
 });
