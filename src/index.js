7b8d3f1e705ea4998c3bdbb05a3232fe0acce7b1
Denis <d.khokhloff2011@yandex.ru>
2022-09-15 14:05:25 +0300

    countries rendering is implemented

--- src/index.js
+++ src/index.js
@@ -10,12 +10,10 @@ import {store} from './store'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,65 @@
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 import { List } from '../components/List';
 import { Card } from '../components/Card';
 import { Controls } from '../components/Controls';
+import { selectAllCountries, selectCountriesInfo } from '../store/countries/countries-selectors';
+import { loadCountries } from '../store/countries/countries-actions'
 
 export const HomePage = () => {
   const navigate = useNavigate();
 
-  const countries = [];
+  const dispatch = useDispatch();
+  const countries = useSelector(selectAllCountries);
+  const {status, error, qty} = useSelector(selectCountriesInfo);
+
+  useEffect(() => {
+    if (!qty) {
+      dispatch(loadCountries());
+    }
+  }, [qty, dispatch]);
 
   return (
     <>
       <Controls />
 
-      <List>
-            {countries.map((c) => {
+      {error && <h2>Can't fetch data</h2>}
+      {status === 'loading' && <h2>Loading...</h2>}
+
+      {status === 'received' && (
+        <List>
+            {countries.map((country) => {
               const countryInfo = {
-                img: c.flags.png,
-                name: c.name,
+                img: country.flags.png,
+                name: country.name,
                 info: [
                   {
                     title: 'Population',
-                    description: c.population.toLocaleString(),
+                    description: country.population.toLocaleString(),
                   },
                   {
                     title: 'Region',
-                    description: c.region,
+                    description: country.region,
                   },
                   {
                     title: 'Capital',
-                    description: c.capital,
+                    description: country.capital,
                   },
                 ],
               };
 
               return (
                 <Card
-                  key={c.name}
-                  onClick={() => navigate(`/country/${c.name}`)}
+                  key={country.name}
+                  onClick={() => navigate(`/country/${country.name}`)}
                   {...countryInfo}
                 />
               );
             })}
-          </List>
+      </List>
+      )}
     </>
   );
 };
